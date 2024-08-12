999fd50e8c28dbe1893308053dd01f0d9dde9643
Được Nguyễn Văn <duocnv@firegroup.io>
2024-08-12 13:01:51 +0700

    refactor(main): group version variables and add explanatory comment

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 	"os"
 )
 
-var version = "dev"
-var commit = "none"
-var date = "unknown"
+// those variables will be set by the build script to the correct values
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
 	cmd.Root.Version = fmt.Sprintf("%s, commit %s, built at %s", version, commit, date)
